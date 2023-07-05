0eb88e5cb986c06e14d674716f52bca903fd85a2
André Marks <andrem4rks@gmail.com>
2023-07-05 13:07:10 -0500

    first commit

---
+++ programacao_assincrona/bin/programacao_assincrona.dart
@@ -0,0 +1,5 @@
+import 'package:programacao_assincrona/programacao_assincrona.dart' as programacao_assincrona;
+
+void main(List<String> arguments) {
+  print('Hello world: ${programacao_assincrona.calculate()}!');
+}
---
+++ programacao_assincrona/test/programacao_assincrona_test.dart
@@ -0,0 +1,8 @@
+import 'package:programacao_assincrona/programacao_assincrona.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
