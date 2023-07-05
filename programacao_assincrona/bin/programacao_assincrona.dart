0786f46561a258405b4571754e27b2d4169981c4
André Marks <andrem4rks@gmail.com>
2023-07-05 16:36:14 -0500

    .

--- programacao_assincrona/bin/programacao_assincrona.dart
+++ programacao_assincrona/bin/programacao_assincrona.dart
@@ -1,14 +1,12 @@
-import 'package:programacao_assincrona/programacao_assincrona.dart'
-    as programacao_assincrona;
 
 void main() {
-  var future = Future.delayed(Duration(seconds: 2));
-
-  future.then((value) => print(4));
-
+  print4();
   print('1');
   print('2');
   print('3');
 
+}
 
+Future<void> print4() async {
+  print('4');
 }
