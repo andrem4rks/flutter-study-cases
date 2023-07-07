5f4b459d366590ffd35b43d3caa1918718fe933a
André Marks <andrem4rks@gmail.com>
2023-07-07 16:03:27 -0500

    execercicio funcoes filtro

---
+++ curso_dart/bin/funcao/filtro_3.dart
@@ -0,0 +1,27 @@
+List<E> filtrar<E>(List<E> lista, Function(E) fn) {
+  List<E> listaFiltrada = [];
+  for(E elemento in lista) {
+    if(fn(elemento)) {
+      listaFiltrada.add(elemento);
+    }
+  }
+  return listaFiltrada;
+}
+
+main() {
+  var notas = [8.2, 7.3, 6.8, 5.4, 2.7, 9.3];
+
+  var boasNotasFn = (double nota) => nota >= 7.5;
+
+  var notasFiltradas = filtrar<double>(notas, boasNotasFn);
+
+  print(notas);
+  print(notasFiltradas);
+
+  var nomes = ['Ana', 'Bia', 'Rebeca', 'Guia', 'João'];
+  var nomesGrandesFn = (String nome) => nome.length >= 5;
+
+  var nomesFiltrados = filtrar<String>(nomes, nomesGrandesFn);
+  print(nomes);
+  print(nomesFiltrados);
+}
