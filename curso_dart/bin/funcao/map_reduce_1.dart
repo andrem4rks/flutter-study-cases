117ca11fa95a0b0a8473f482a251b272be29fd93
André Marks <andrem4rks@gmail.com>
2023-07-07 17:17:42 -0500

    exercicios map e reduce

---
+++ curso_dart/bin/funcao/map_reduce_1.dart
@@ -0,0 +1,21 @@
+main() {
+  var alunos = [
+    {'nome': 'Alfredo', 'nota': 9.9},
+    {'nome': 'Wilson', 'nota': 9.3},
+    {'nome': 'Mariana', 'nota': 8.7},
+    {'nome': 'Guilherme', 'nota': 8.1},
+    {'nome': 'Ana', 'nota': 7.6},
+    {'nome': 'Ricardo', 'nota': 6.8},
+  ];
+
+  String Function(Map) pegarApenasONomeFn = (aluno) => aluno['nome'];
+  int Function(String) qtdLetrasFn = (texto) => texto.length;
+  int Function(int) qtdLetrasX2Fn = (qtd) => qtd * 2;
+
+  var resultado = alunos
+    .map(pegarApenasONomeFn)
+    .map(qtdLetrasFn)
+    .map(qtdLetrasX2Fn);
+  
+  print(resultado);
+}
